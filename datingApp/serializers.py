ff9e2ee4c2939079c86ab2f462573623ea28477b
emily <75251243+shyiyan@users.noreply.github.com>
2023-12-20 18:16:17 +0800

    connect to mysql

--- datingApp/serializers.py
+++ datingApp/serializers.py
@@ -1,8 +1,19 @@
 # datingApp/serializers.py
 from rest_framework import serializers
 from .models import User
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'bio', 'firstName', 'lastName', 'email', 'profilePicture')
+"""
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True, 
+        validators=[UniqueValidator(queryset=User.objects.all())]
+                                   )
+ """
--- datingApp/settings.py
+++ datingApp/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'datingApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'datingApp',
+        'USER': 'root',
+        'PASSWORD': '2502',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
